abfaf2b58373f5f65e43ed23df581409e0262d63
Remigiusz Wozniak <remek778790@gmail.com>
2022-05-07 15:48:46 +0200

    router refresh error 404 fix

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/pet-store">
     <App />
   </BrowserRouter>,
   document.getElementById("root")
